4c1601784e9ff52f83c471a82c497998112c11db
camzakkour <camzakkour@gmail.com>
2022-02-24 11:17:27 -0800

    redeployment

--- server/index.js
+++ server/index.js
@@ -1,6 +1,16 @@
 const express = require('express')
 const path = require('path')
 
+const Rollbar = require('rollbar')
+const rollbar = new Rollbar({
+  accessToken: 'f240c13e6e5347c6aa6b6bcc31caddd6',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
+// record a generic message and send it to Rollbar
+rollbar.log('Hello world!')
+
 const app = express()
 
 app.get('/', (req, res) => {
